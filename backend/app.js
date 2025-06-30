15e226b7fd209dcb3650bd33577abd86d5891cb9
Louis2324 <miguellouis2023@gmail.com>
2025-06-30 19:42:06 +0200

    Added static frontend

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import scraperRoutes from "./routes/scraper.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors());
+app.use(express.static(path.join(__dirname, "..","public")));
 app.use(express.json());
 app.use("/api",scraperRoutes);
-
-app.get("/",(req,res)=>{
-    res.send("Webtoon scraper is running live");
-})
 
 export default app;
